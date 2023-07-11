84cbf8b85d66d189016994d667be44da335832b1
jhorn00 <dawshorn@gmail.com>
2023-07-10 23:53:53 -0400

    Simple how-to home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,17 +2,18 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { NavBar } from "./components/NavBar/NavBar";
+import Home from "./components/Home/Home";
+import Profile from "./components/Profile/Profile";
 import NetWorth from "./components/NetWorth/NetWorth";
 import Balance from "./components/Balance/Balance";
 import MonthlyExpenses from "./components/MonthyExpenses/MonthlyExpenses";
-import Profile from "./components/Profile/Profile";
 
 export default function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
+        <Route path="/" element={<Home />} />
         <Route path="profile" element={<Profile />} />
         <Route path="networth" element={<NetWorth />} />
         <Route path="balance" element={<Balance />} />
