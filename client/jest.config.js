6d56a13e1b1122629546950664bfa740413e8058
jhorn00 <dawshorn@gmail.com>
2023-07-14 19:19:47 -0400

    try another config

--- client/jest.config.js
+++ client/jest.config.js
@@ -2,10 +2,7 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
     "transform": {
-        "node_modules/variables/.+\\.(j|t)sx?$": "ts-jest"
+        '^.+\\.(j|t)sx?$': 'ts-jest',
     },
-    "transformIgnorePatterns": [
-        "node_modules/(?!variables/.*)"
-    ]
 };
   
