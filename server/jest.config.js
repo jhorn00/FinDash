088e3220a6827f88ab861919b066b5b1dc9674fd
jhorn00 <dawshorn@gmail.com>
2023-07-14 19:04:28 -0400

    last commit made a change to the server config instead, revert

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,11 +1,5 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    "transform": {
-        "node_modules/variables/.+\\.(j|t)sx?$": "ts-jest"
-    },
-    "transformIgnorePatterns": [
-        "node_modules/(?!variables/.*)"
-    ]
 };
   
