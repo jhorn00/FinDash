46b2a83800e78a17e3a70be005659af1b6ec4b80
jhorn00 <dawshorn@gmail.com>
2023-07-14 20:18:28 -0400

    fix testing on client side

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
+    preset: "ts-jest",
+    testEnvironment: "jsdom",
     "transform": {
-        '^.+\\.(j|t)sx?$': 'ts-jest',
+        "^.+\\.(j|t)sx?$": "ts-jest",
+        ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "jest-transform-stub",
     },
 };
   
---
+++ client/test/App.test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import "./mock/matchMedia.mock";
+import Home from "../src/components/Home/Home";
+
+// This is just a test.......... test.
+test("renders home page and checks for FinDash", () => {
+  render(<Home />);
+  // Check that home has the text "FinDash" in it
+  const linkElement = screen.getByText(/FinDash/i);
+  expect(linkElement).toBeInTheDocument();
+});
