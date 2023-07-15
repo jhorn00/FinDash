4bd44751eb3939e1bb571355380fbf4f824619ca
jhorn00 <dawshorn@gmail.com>
2023-07-15 19:03:41 -0400

    update disclaimer text

--- client/src/components/Disclaimer/Disclaimer.tsx
+++ client/src/components/Disclaimer/Disclaimer.tsx
@@ -9,16 +9,15 @@ function Disclaimer() {
         This application is a work-in-progress and is hosted for demonstration
         purposes only.{" "}
         <a href="https://github.com/jhorn00" style={{ textDecoration: "none" }}>
-          The author{" "}
+          The author of this application{" "}
         </a>
-        of this application is not responsible for any use of this application
-        with real financial data, nor do they want your financial information.
+        bears no responsibility for any use of this application with real
+        financial data.
       </p>
       <p className="disclaimer-text">
-        It is recommended to only upload fake data, as anyone with the link can
-        view your dates/transactions. For real transaction data, please
-        self-host FinDash on a development environment or local release
-        environment.
+        Therefore, it is recommended to upload only fake data, as anyone with
+        the link can view your dates/transactions. For real transaction data,
+        please self-host FinDash on a development or local release environment.
       </p>
     </div>
   );
